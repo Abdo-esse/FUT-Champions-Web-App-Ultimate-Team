478359dd949fb9508d322868f93d0b0942d67971
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-27 23:11:59 +0100

    j'ai la fonction de enregitredata

--- asset/js/validationDuForm.js
+++ asset/js/validationDuForm.js
@@ -2,111 +2,134 @@
 
 // les input du condition phisiquale
 
-let conditionPhisiqueIunput = document.querySelectorAll('.conditionPhisiqueIunput')
+let conditionPhisiqueIunput = document.querySelectorAll( ".conditionPhisiqueIunput");
+console.log(conditionPhisiqueIunput);
+
 
 //les inpits du text
 
-let inputsText=document.querySelectorAll('.inputsText')
-
-
+let inputsText = document.querySelectorAll(".inputsText");
 
 //Button de send data
- const sendDataBtn= document.querySelector('.sendData')
+const sendDataBtn = document.querySelector(".sendData");
 
-//les inputse d'image 
-let inputsLien=document.querySelectorAll('.inputsLien')
+//les inputse d'image
+let inputsLien = document.querySelectorAll(".inputsLien");
 
+//data
+let data = [];
+// fullNam
+let fullName,
+  photo,
+  position,
+  flag,
+  club,
+  logo,
+  rating,
+  pace,
+  shooting,
+  passing,
+  dribbling,
+  defending,
+  physical;
 
- //file
-//  const image = document.getElementById(multiple_logo)
-  
- const imageRegex = /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i;
+  //la formul de regix de valides un text
+  const regex = /^[a-zA-ZÀ-ÿ\s'-]+$/;
 
-//les fonction 
+//les fonction
 
 // les fonction de validation des inputs
-
-//validation condition Phisique
+ //validation condition Phisique
  function validationconditionPhisique() {
-    
     for (let i = 0; i < conditionPhisiqueIunput.length; i++) {
-      
-      if (conditionPhisiqueIunput[i].value > 0 && conditionPhisiqueIunput[i].value<100) {
+      if (conditionPhisiqueIunput[i].value > 0 &&conditionPhisiqueIunput[i].value < 100) {
+        conditionPhisiqueIunput[i].style.border = "2.5px solid green";
         
-        conditionPhisiqueIunput[i].style.border="2.5px solid green"
-      }
-      else  {
-         conditionPhisiqueIunput[i].style.border="2.5px solid red"
+      } else {
+        conditionPhisiqueIunput[i].style.border = "2.5px solid red";
+        
+
       }
     }
-}
+  }
+
 //valides des inputs text
 function validationText() {
-   
-    const regex = /^[a-zA-ZÀ-ÿ\s'-]+$/;
     for (let i = 0; i < inputsText.length; i++) {
-        if (regex.test(inputsText[i].value)) {
-        
-            inputsText[i].style.border="2.5px solid green"
-        }
-        else{
-            
-            inputsText[i].style.border="2.5px solid red"
-         }     
+      if (regex.test(inputsText[i].value)) {
+        inputsText[i].style.border = "2.5px solid green";
+      } 
+      else {
+        inputsText[i].style.border = "2.5px solid red";
+      }
     }
-}
+  }
 
-//validation des inputs image
+
+  //validation des inputs image
 function validationImage() {
     const regexImage = /(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg|webp))/gi;
-    console.log('hfhf');
-    
+  
     for (let i = 0; i < inputsLien.length; i++) {
-        if (regexImage.test(inputsLien[i].value)) {
-            console.log("nadi");
-            inputsLien[i].style.border="2.5px solid green"
-        }
-        else{
-            console.log('node tg3d');
-            inputsLien[i].style.border="2.5px solid red"
-         }     
+      if (regexImage.test(inputsLien[i].value)) {
+        inputsLien[i].style.border = "2.5px solid green";
+      } else {
+        inputsLien[i].style.border = "2.5px solid red";
+      }
     }
-    //  const image = document.querySelector(".multiple_logo").files
-// if (image) {
-//     const urlsd = URL.createObjectURL(image)
+  }
 
-    
-// }
- 
 
- 
-
-    
+function enregitrerData() {
+    for (let i = 0; i < conditionPhisiqueIunput.length; i++) {
+        if (conditionPhisiqueIunput[i].value > 0 &&conditionPhisiqueIunput[i].value < 100) {
+          rating=conditionPhisiqueIunput[0].value
+          console.log(rating);
+          pace=conditionPhisiqueIunput[1].value
+          shooting=conditionPhisiqueIunput[2].value
+          passing=conditionPhisiqueIunput[3].value
+          dribbling=conditionPhisiqueIunput[4].value
+          defending=conditionPhisiqueIunput[5].value
+          physical=conditionPhisiqueIunput[6].value
+          
+        }
+    for (let i = 0; i < inputsText.length; i++) {
+      if (regex.test(inputsText[i].value)) {
+        fullName=inputsText[0].value
+        // name=
+//   photo,
+//   position,
+//   flag,
+//   club,
+      }
+    }
+    }
+   
+   
+      
+  
+  
 }
 
+// l'appel des fonctions
 
-
-
- // l'appel des fonctions
-
- //validation
- //validation de nombre
- for (let i = 0; i < conditionPhisiqueIunput.length; i++) {
-    conditionPhisiqueIunput[i].addEventListener('input',validationconditionPhisique)
-    
- }
+//validation
+//validation de nombre
+for (let i = 0; i < conditionPhisiqueIunput.length; i++) {
+  conditionPhisiqueIunput[i].addEventListener(
+    "input",
+    validationconditionPhisique
+  );
+}
 //validation de text
- for (let i = 0; i < inputsText.length; i++) {
-    inputsText[i].addEventListener('input',validationText)
-    
- }
- //validation de lien 
- for (let i = 0; i < inputsLien.length; i++) {
-    inputsLien[i].addEventListener('input',validationImage)
-
- }
-
+for (let i = 0; i < inputsText.length; i++) {
+  inputsText[i].addEventListener("input", validationText);
+}
+//validation de lien
+for (let i = 0; i < inputsLien.length; i++) {
+  inputsLien[i].addEventListener("input", validationImage);
+}
 
 //  conditionPhisiqueIunput.addEventListener('change',validationInputs)
 
-// sendDataBtn.addEventListener('click',validationText)
+sendDataBtn.addEventListener("click",enregitrerData);
