664015002789b4d7356268d37161bed5e779023e
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-28 14:50:34 +0100

    j'ai termin la function de formulair

--- asset/js/fourmedynamique.js
+++ asset/js/fourmedynamique.js
@@ -1,2 +1,27 @@
-import{data,localStorageData} from "./validationDuForm.js"
+import{selectInput} from "./validationDuForm.js"
 
+//div des inputs du GK
+let golKepr=document.querySelector('.goolkepr')
+
+
+//div des inputs du joueurs
+let joueur=document.querySelector('.joueurs')
+
+
+
+function formedynamiq() {
+    if (selectInput.value=="GK"){
+        golKepr.style.display="block"
+        joueur.style.display="none"
+     }else if (selectInput.value=="") {
+        golKepr.style.display="none"
+        joueur.style.display="none"
+     }else{
+         joueur.style.display="block"
+         golKepr.style.display="none"
+     }
+      
+}
+
+
+selectInput.addEventListener('change',formedynamiq)
