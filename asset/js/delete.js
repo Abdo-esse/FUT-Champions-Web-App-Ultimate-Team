99413cd1177cefebe1fc6dd14a910941f7e069ca
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-29 11:08:22 +0100

    j'ai terin la function de delet

---
+++ asset/js/delete.js
@@ -0,0 +1,11 @@
+import{data,localStorageData} from "./validationDuForm.js"
+import{ displayCard} from "./displayCard.js"
+
+function deleteJoueur(indexDelet) {
+    
+    data.splice(indexDelet,1)
+    displayCard()
+    localStorageData()
+}
+
+export{ deleteJoueur}
